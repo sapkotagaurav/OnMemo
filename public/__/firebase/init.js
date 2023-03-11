93d5477810dbdf28543046eb379a16a16c9dc758
Gaurab Sapkota <g@pop-os.localdomain>
2023-03-10 18:00:24 -0600

    making drastci changes

---
+++ public/__/firebase/init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyArn_g43HcaTTnCBUgmXsmP9jSeCroz2Y8",
+  "appId": "1:673863465917:web:ca6306c270631b78e0b0d4",
+  "authDomain": "memosonline-2a6da.firebaseapp.com",
+  "databaseURL": "https://memosonline-2a6da.firebaseio.com",
+  "measurementId": "G-N4HW9KLM2Q",
+  "messagingSenderId": "673863465917",
+  "projectId": "memosonline-2a6da",
+  "storageBucket": "memosonline-2a6da.appspot.com"
+});
